ad947628a0ed821f805f7749d797359e10d7a40c
AlaaHany2003 <hanyalaahany2003@gmail.com>
2025-06-10 11:16:23 +0300

    users api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\APIs\UserController;
 use App\Http\Controllers\user\HomeController;
 use App\Http\Controllers\APIs\ProductController;
 use App\Http\Controllers\APIs\CategroyController;
