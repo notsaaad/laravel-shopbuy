3fb7e7a043e010a2753bc28d685a4ba238af2e1e
notsaaad <https://github.com/notsaaad/sawyancom.git>
2025-06-20 21:22:02 +0300

    Sinle Product Api

--- app/Http/Controllers/APIs/ProductController.php
+++ app/Http/Controllers/APIs/ProductController.php
@@ -11,7 +11,7 @@ use App\Http\Controllers\Controller;
 class ProductController extends Controller
 {
   function index(){
-    $Product = Product::where('is_draft', 1)->get();
+    $Product = Product::where('is_draft', 0)->get();
 
     foreach ($Product as $q) {
       $path       = ProductImagePath() . $q->image;
@@ -27,7 +27,7 @@ class ProductController extends Controller
 
   function category($id){
     $category  = Category::find($id);
-    $product   = $category->products()->where('is_draft', 1);
+    $product   = $category->products()->where('is_draft', 0);
 
     foreach ($product as $q) {
       $path       = ProductImagePath() . $q->image;
@@ -39,5 +39,71 @@ class ProductController extends Controller
       'message' => 'Products retrieved successfully.',
       'data'    => $product,
     ], 200);
+  }
+
+
+  public function single_product($id){
+      $product = Product::findOrFail($id);
+
+      $type = $product->variants()->exists() ? 'variant' : 'simple';
+
+
+      $gallery = json_decode($product->gallery, true);
+
+      // توليد روابط URL كاملة باستخدام Storage
+      $gallary = collect($gallery)->map(function ($img) {
+          $path       = ProductImagePath() .$img;
+          return URL::asset($path);
+      });
+
+      $imagePath = ProductImagePath() .$product->image;
+      $image     = URL::asset($imagePath);
+
+      $attributes = [];
+      if ($type === 'variant') {
+          $product->variants->each(function ($variant) use (&$attributes) {
+              foreach ($variant->attributeValues as $value) {
+                  $attrName = $value->attribute_name;
+                  $attrVal = $value->value;
+                  $attributes[$attrName][] = $attrVal;
+              }
+          });
+
+
+          foreach ($attributes as $key => $vals) {
+              $attributes[$key] = array_unique($vals);
+          }
+      }
+
+
+      $variantList = [];
+      if ($type === 'variant') {
+          $variantList = $product->variants->map(function ($variant) {
+              $attrs = [];
+              foreach ($variant->attributeValues as $val) {
+                  $attrs[$val->attribute_name] = $val->value;
+              }
+
+              return [
+                  'id' => $variant->id,
+                  'attributes' => $attrs,
+                  'price' => $variant->price,
+                  'stock' => $variant->stock
+              ];
+          });
+      }
+
+      return response()->json([
+          'id' => $product->id,
+          'type' => $type,
+          'name' => $product->title,
+          'description' => $product->description ?? '',
+          'price' => $product->price,
+          'image' => $image,
+          'sale' => $product->sale,
+          'gallary' => $gallary,
+          'attributes' => $attributes,
+          'variants' => $variantList,
+      ]);
   }
 }
